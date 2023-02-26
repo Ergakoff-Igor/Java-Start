5a655c04168574e21a8a9b4239f749b40e89514a
Igor <i.ergakoff@mail.ru>
2023-02-26 21:19:44 +0300

    Java. Homework 4

---
+++ 8. Java знакомство и как пользоваться базовым API/Task/Seminar_4/Homework4.java
@@ -0,0 +1,74 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+
+public class Homework4 {
+    public static void main(String[] args) {
+
+        HashMap<String,HashSet<Integer>> phoneBook = new HashMap<>();
+
+        menu(phoneBook);
+
+    }
+
+    static void menu(HashMap<String, HashSet<Integer>> phoneBook) {
+
+        System.out.println("Введите номер операции для выбора действия: \n" +
+                "1. Добавить контакт.\n" +
+                "2. Найти контакт.\n" +
+                "3. Кдалить контакт.\n");
+
+        int num = readInt();
+
+        switch (num) {
+            case 1 -> addNumber(phoneBook);
+            case 2 -> findNumber(phoneBook);
+            case 3 -> deleteNumber(phoneBook);
+        }
+    }
+
+    static void deleteNumber(HashMap<String, HashSet<Integer>> phoneBook) {
+
+        System.out.println("введите имя контакта для удаления.");
+        String name = readStr();
+        phoneBook.remove(name);
+    }
+
+    static void findNumber(HashMap<String, HashSet<Integer>> phoneBook) {
+        System.out.println("Введите имя контакта для поиска.");
+        String name = readStr();
+
+        HashSet<Integer> value = phoneBook.get(name);
+
+        System.out.println(value);
+    }
+
+    static void addNumber(HashMap<String, HashSet<Integer>> phoneBook) {
+
+        System.out.println("Введите имя.");
+        String name = readStr();
+        System.out.println("Введите номер.");
+        int number = readInt();
+
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(number);
+        phoneBook.put(name, numbers);
+        System.out.println(phoneBook);
+    }
+
+    static int readInt() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int num = scanner.nextInt();
+            return num;
+        }
+    }
+    static String readStr() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String name = scanner.nextLine();
+            return name;
+        }
+    }
+
+}
+
